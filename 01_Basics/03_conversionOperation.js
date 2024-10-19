548557ac132ba9e33d0eb622b65a85dfdd2e6e1a
vishvajit <wanivishvajit1234@gmail.com>
2024-10-19 11:54:13 +0530

    Conversion of DataTypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = "Hitesh";
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueUnNumber = Number(score);
+//console.log(typeof valueUnNumber);
+//console.log(valueUnNumber);
+
+
+// notes 
+// "33" => 33
+// "33bvc" => NaN   not a number
+// true => 1 ; false => 0
+
+
+let isLoggedIn = "vishva";
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// notes
+// 1 => true ; 0 => false
+//  "" => false
+// "vishva" => true
+
+// ---------------------------------------------------------------------------------------
+let someNumber = 55;
+
+console.log(typeof someNumber);
+console.log(someNumber);
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+console.log(someNumber);
