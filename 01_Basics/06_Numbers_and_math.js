4d2487df28f49b672d023c6b6b72a1c7195f79f3
vishvajit <wanivishvajit1234@gmail.com>
2024-11-24 12:17:16 +0530

    Numbers_Math

---
+++ 01_Basics/06_Numbers_and_math.js
@@ -0,0 +1,35 @@
+const score = 400;
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 2334.38;
+// console.log(otherNumber.toPrecision(5));
+
+
+const hundreds = 100000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//////////////////////////////////////////////////////////////////////////
+
+// console.log(Math);
+// console.log(Math.abs(-4));   //convert -val into + val
+// console.log(Math.round(4.7));
+// console.log(Math.round(4.2));
+// console.log(Math.ceil(8.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,6,9,1,10));
+// console.log(Math.max(4,6,9,1,10)); 
+
+
+// console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10 ;
+const max = 20 ;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+ 
