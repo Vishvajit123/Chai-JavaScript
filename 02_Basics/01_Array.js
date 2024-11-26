beaa92a44c8247109c876297c2bb5397f5ba1fba
vishvajit <wanivishvajit1234@gmail.com>
2024-11-26 12:08:17 +0530

    arrayBasics1

---
+++ 02_Basics/01_Array.js
@@ -0,0 +1,47 @@
+const myArr = [0, 1, 2, 3, 4, 5];
+console.log(myArr);
+
+//  const myHeros = ["vishva", "jayesh","devika"];
+// console.log(myHeros);
+
+ const myArr2 = new Array(10, 30, 33 , 47);
+// console.log(myArr2[3])
+
+
+
+//Array Methods
+// myArr.push(6);  //add ele at last
+// myArr.push(7);
+// myArr.pop();    //del last ele of array
+
+// myArr.unshift(8);    // add ele at starting
+// console.log(myArr);
+// myArr.shift();       //delete first ele in array
+// console.log(myArr);
+
+// console.log(myArr.includes(5));   //includes method checks for existing ele is present or not
+// console.log(myArr.indexOf(5));    // it will give u the index of that element 
+// console.log(myArr.indexOf(10));   //if that element is not present then it will give u -1 
+
+
+// const newArr = myArr.join();     //it will bind the array and convert into string
+// console.log(myArr);
+// console.log(newArr);     
+// console.log(typeof(newArr));
+
+
+
+////slice and splice
+// const myNewArray = myArr.slice(1, 3);
+// console.log("A" , myArr);  // [0, 1, 2, 3, 4, 5]     
+// console.log(myNewArray);   //o/p - 1,2
+
+// const myNewArray2 = myArr.splice(1, 3);
+// console.log("B" ,myArr);   // [0, 4, 5]
+// console.log(myNewArray2);  //op - 1, 2, 3
+
+// // difference in splice and splice 
+// // 1) output difference 
+// // 2) slice will not change the orignal array but it splice will change the orignal array 
+
+
